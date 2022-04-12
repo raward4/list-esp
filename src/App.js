904f319ced89bbffc8d525531ce2f349869a6b72
Robert Ward <raward4@ncsu.edu>
2022-04-12 12:03:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    skills: [{ skill: "JavaScript", level: 4 }],
+    // New state for the inputs below
+    skill: "",
+    level: 3
+  };
+
+  addSkill = () => {
+    alert("ADD SKILL CLICKED");
+  };
+
+  render() {
+    return (
+      <section>
+        <h2>DEV SKILLS</h2>
+        <hr />
+        {this.state.skills.map((s) => (
+          <article key={s.skill}>
+            <div>{s.skill}</div> <div>{s.level}</div>
+          </article>
+        ))}
+        <hr />
+        <form>
+          <label>
+            <span>SKILL</span>
+            <input name="skill" />
+          </label>
+          <label>
+            <span>LEVEL</span>
+            <select name="level">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </label>
+          <button onClick={this.addSkill}>ADD SKILL</button>
+        </form>
+      </section>
+    );
+  }
 }
 
 export default App;
