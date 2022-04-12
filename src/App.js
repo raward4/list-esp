2bbfa6f1c7ea1c95a354527ddbc23bb434996b07
Robert Ward <raward4@ncsu.edu>
2022-04-12 13:02:35 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -10,13 +10,22 @@ class App extends Component {
   };
 
   addSkill = () => {
-    alert("ADD SKILL CLICKED");
+    // Using the "function" approach because relying on existing state
+    this.setState(state => ({
+      // Always replace, don't mutate top-level state properties
+      skills: [...state.skills, state.newSkill],
+      // Reset the inputs for better UX
+      newSkill: {skill: '', level: 3}
+    }));
   };
-
+  
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
       <section>
-        <h2>DEV SKILLS</h2>
+        <h2>Skills</h2>
         <hr />
         {this.state.skills.map((s) => (
           <article key={s.skill}>
@@ -24,22 +33,28 @@ class App extends Component {
           </article>
         ))}
         <hr />
-        <form>
+        <form onSubmit={this.addSkill = e => {
+          e.preventDefault();
+          }
+        } id="events">
           <label>
-            <span>SKILL</span>
-            <input name="skill" />
-          </label>
-          <label>
-            <span>LEVEL</span>
-            <select name="level">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
+            <span>Add Event</span>
+            <input name="skill" 
+            value={this.state.skill}
+            onChange={this.handleChange} 
+            />
+            </label>
+            <label>
+            <span></span>
+            <select name="level" 
+            value={this.state.level}    
+            onChange={this.handleChange}>
+              <option value="1">Feeding</option>
+              <option value="2">Nap</option>
+              <option value="3">Diaper</option>
             </select>
           </label>
-          <button onClick={this.addSkill}>ADD SKILL</button>
+          <button onClick={this.addSkill}>Add to Log</button>
         </form>
       </section>
     );
